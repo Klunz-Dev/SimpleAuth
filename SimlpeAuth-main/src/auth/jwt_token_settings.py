206877f0dce85f8b4976d80d4fe683cbded6a436
Farhat Ganiev <153320105+Klunz-Dev@users.noreply.github.com>
2026-03-08 13:53:07 +0300

    Update jwt_token_settings.py

--- SimlpeAuth-main/src/auth/jwt_token_settings.py
+++ SimlpeAuth-main/src/auth/jwt_token_settings.py
@@ -1,8 +1,9 @@
 from authx import AuthX, AuthXConfig
 
 config = AuthXConfig(
-    JWT_SECRET_KEY='MgXMlqSHxb1bUJkhE9b68BsonWMuQdJR',
+    JWT_SECRET_KEY='',
     JWT_TOKEN_LOCATION=['cookies']
 )
 
 security = AuthX(config=config)
+
